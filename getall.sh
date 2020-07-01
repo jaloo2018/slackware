1fd9e069b3966ddc4b578230e61920a70d226d9e
刘家禄 <liu.jialu@eco-ev.com>
2020-07-01 11:11:52 +0800

    add slackware64-14.2

--- getall.sh
+++ getall.sh
@@ -2,6 +2,9 @@
 # for slackware64-current
 rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-current .
 
+# for slackware64-14.2
+rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-14.2 .
+
 # for slackwarearm
 # URL: arm.slackware.com/getslack/
 rsync -Pavv --delete ftp.arm.slackware.com::slackwarearm/slackwarearm-current .
