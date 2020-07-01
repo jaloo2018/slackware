0791a95683105060a49dfb680b8679492f35cb26
刘家禄 <liu.jialu@eco-ev.com>
2020-07-01 10:27:52 +0800

    add slackwarearm

--- getall.sh
+++ getall.sh
@@ -2,6 +2,10 @@
 # for slackware64-current
 rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-current .
 
+# for slackwarearm
+# URL: arm.slackware.com/getslack/
+rsync -Pavv --delete ftp.arm.slackware.com::slackwarearm/slackwarearm-current .
+
 # Slackware Linux Essentials
 # wget ftp://ftp.slackbook.org/slackbook/slackbook-2.0.pdf
 # okular slackbook-2.0.pdf
