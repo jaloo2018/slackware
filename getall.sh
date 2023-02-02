9fa857b3cfc798fdc9553e1dfa64598a3f3f17bf
刘家禄 <liu.jialu@eco-ev.com>
2023-02-02 17:56:45 +0800

    update

--- getall.sh
+++ getall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+# for slackware64-15.0
+rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-15.0 .
+
 # for slackware64-current
 rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-current .
 
-# for slackware64-14.2
-rsync -avP --delete rsync://ftp.slackware.com/slackware/slackware64-14.2 .
-
 # for slackwarearm
 # URL: arm.slackware.com/getslack/
-rsync -Pavv --delete ftp.arm.slackware.com::slackwarearm/slackwarearm-current .
+# rsync -Pavv --delete ftp.arm.slackware.com::slackwarearm/slackwarearm-current .
 
 # Slackware Linux Essentials
 # wget ftp://ftp.slackbook.org/slackbook/slackbook-2.0.pdf
